859843677a9807195dac3f28c430913ec4ed6548
Chouaib <chouaib.skt@gmail.com>
2023-12-21 15:11:15 +0100

    hide/show toggle

--- interfaces/player_choice.py
+++ interfaces/player_choice.py
@@ -1,23 +1,30 @@
-from interfaces.players_list import players_list
 import tkinter
 from tkinter import *
-from tkinter import ttk 
+from tkinter import ttk
+from interfaces.players_list import players_list
+
 def init_window(window):
+    def hide_player_choice():
+        lbl_number_player.pack_forget()
+        combo_number_player.pack_forget()
+        btnValider.pack_forget()
+
+    def reinit_player_choice():
+        hide_player_choice()
+        init_window(window)
+
     def AffichageJoueur(nb):
         nb = int(nb)
-        players_list(nb,window)
+        players_list(nb, window, reinit_player_choice)
 
-    # Selection of the numbers of players
+    # Selection of the number of players
     lbl_number_player = tkinter.Label(window, text="Choisissez le nombre de joueurs :")
     lbl_number_player.pack()
 
-    list_of_number_player = [1,2, 3, 4]
+    list_of_number_player = [1, 2, 3, 4]
     combo_number_player = ttk.Combobox(window, values=list_of_number_player, state="readonly")
-
-    # Select the current element
     combo_number_player.current(0)
     combo_number_player.pack()
 
-    # dès que le bouton est cliqué on renvoie le nombre de joueur choisit
-    btnValider = Button(window, text="Bowling score manager ultimate",command = lambda:AffichageJoueur(combo_number_player.get()) )
+    btnValider = Button(window, text="Bowling score manager ultimate", command=lambda: [hide_player_choice(), AffichageJoueur(combo_number_player.get())])
     btnValider.pack()
--- interfaces/players_list.py
+++ interfaces/players_list.py
@@ -1,30 +1,28 @@
-from . import config 
 import tkinter
 from tkinter import *
-from tkinter import ttk 
-window = config.window
-canvas = config.canvas
+from tkinter import ttk
 
-#fonction de récupération du pseudo du joueur
-def playParty(entries):
+def players_list(count, window, callback):
+    def show_player_choice():
+        for widget in frame.winfo_children():
+            widget.destroy()
+        frame.pack_forget()
+        callback()
+
+    def playParty(entries):
         for i, entry in enumerate(entries):
             print(f"Player {i + 1}: {entry.get()}")
 
-def players_list(count,window):
-
-    # Centrer les widgets dans la fenêtre
+    # Centering the widgets in the window
     frame = ttk.Frame(window)
     frame.place(relx=0.5, rely=0.5, anchor='center')
 
-    # Liste pour stocker les objets Entry
     entries = []
-
-    # Définir les étiquettes et les entrées pour les joueurs en utilisant une boucle
     for i in range(count):
         ttk.Label(frame, text=f'Player {i + 1}:').grid(row=i, column=0, padx=10, pady=10)
         entry = ttk.Entry(frame, width=20)
         entry.grid(row=i, column=1, padx=10, pady=10)
         entries.append(entry)
 
-    # Ajouter le bouton de validation
-    ttk.Button(frame, text="Validate", width=20,command = lambda:playParty(entries)).grid(row=3, column=0, columnspan=2, pady=10)
+    ttk.Button(frame, text="Validate", width=20, command=lambda: playParty(entries)).grid(row=count, column=0, columnspan=2, pady=10)
+    ttk.Button(frame, text="Back", width=20, command=show_player_choice).grid(row=count+1, column=0, columnspan=2, pady=10)
