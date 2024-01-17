02aed58eab9406dad036ddab305de59add46ed2a
HamCam203 <camarahamoudia@gmail.com>
2024-01-17 09:03:51 +0100

    modif name var

--- interfaces/play_party.py
+++ interfaces/play_party.py
@@ -8,7 +8,7 @@ from package_party.class_player import *
 window = config.window
 canvas = config.canvas
 
-def play_party(entries, nb_tours, nb_bowling, window):
+def play_party(entries, nb_tours, nb_skittles, window):
     # Create a frame to hold the widgets
     frame = ttk.Frame(window)
     frame.place(relx=0.5, rely=0.5, anchor='center')
@@ -25,12 +25,12 @@ def play_party(entries, nb_tours, nb_bowling, window):
     lbl_quille = tkinter.Label(frame, text="Nombre de quilles tombé")
     lbl_quille.grid(row=2, column=1, padx=10, pady=10)
 
-    # Combobox for the number of fallen bowling pins
-    list_of_number_falling_bowling = []
-    for i in range(int(nb_bowling), 0, -1):
-        list_of_number_falling_bowling.append(i)
+    # Combobox for the number of fallen skittles pins
+    list_of_number_falling_skittles = []
+    for i in range(int(nb_skittles), 0, -1):
+        list_of_number_falling_skittles.append(i)
 
-    combo_Score = ttk.Combobox(frame, values=list_of_number_falling_bowling, state="readonly")
+    combo_Score = ttk.Combobox(frame, values=list_of_number_falling_skittles, state="readonly")
     combo_Score.grid(row=2, column=2, padx=10, pady=10)
 
     # Score
